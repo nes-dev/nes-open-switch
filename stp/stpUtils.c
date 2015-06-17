8742177261b48f47f81a04176045267634979afa
NES <nes-repo@users.noreply.github.com>
2015-06-18 04:23:13 +0530

    [nes]: STP changes ...

--- stp/stpUtils.c
+++ stp/stpUtils.c
@@ -33,6 +33,46 @@
 #include <stdint.h>
 
 
+enum
+{
+	ieee8021StpPortId_portNumberMask_c = 0x0FFF,
+	
+	ieee8021StpBridgeId_addressOffset_c = 2,
+};
+
+
+#define ieee8021StpPortId_portNumber(_portId)	((_portId) & ieee8021StpPortId_portNumberMask_c)
+
+#define ieee8021StpBridgeId_address(_bridgeId)		(&(_bridgeId)[ieee8021StpBridgeId_addressOffset_c])
+
+
+int8_t
+ieee8021StpPathVector_cmp (
+	uint8_t *pu8CstRootId1, uint32_t u32CstPathCost1,
+	uint8_t *pu8MstRootId1, uint32_t u32MstPathCost1, uint8_t *pu8TxBridgeId1, uint16_t u16TxPortId1, uint16_t u16RxPortId1,
+	uint8_t *pu8CstRootId2, uint32_t u32CstPathCost2,
+	uint8_t *pu8MstRootId2, uint32_t u32MstPathCost2, uint8_t *pu8TxBridgeId2, uint16_t u16TxPortId2, uint16_t u16RxPortId2)
+{
+	register int8_t i8Cmp =
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) < 0) ||
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 < u32CstPathCost2) ||
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 == u32CstPathCost2 && memcmp (pu8MstRootId1, pu8MstRootId2, 8) < 0) ||
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 == u32CstPathCost2 && memcmp (pu8MstRootId1, pu8MstRootId2, 8) == 0 && u32MstPathCost1 < u32MstPathCost2) ||
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 == u32CstPathCost2 && memcmp (pu8MstRootId1, pu8MstRootId2, 8) == 0 && u32MstPathCost1 == u32MstPathCost2 && memcmp (pu8TxBridgeId1, pu8TxBridgeId2, 8) < 0) ||
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 == u32CstPathCost2 && memcmp (pu8MstRootId1, pu8MstRootId2, 8) == 0 && u32MstPathCost1 == u32MstPathCost2 && memcmp (pu8TxBridgeId1, pu8TxBridgeId2, 8) == 0 && u16TxPortId1 < u16TxPortId2) ||
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 == u32CstPathCost2 && memcmp (pu8MstRootId1, pu8MstRootId2, 8) == 0 && u32MstPathCost1 == u32MstPathCost2 && memcmp (pu8TxBridgeId1, pu8TxBridgeId2, 8) == 0 && u16TxPortId1 == u16TxPortId2 && u16RxPortId1 < u16RxPortId2) ? -1:
+		(memcmp (pu8CstRootId1, pu8CstRootId2, 8) == 0 && u32CstPathCost1 == u32CstPathCost2 && memcmp (pu8MstRootId1, pu8MstRootId2, 8) == 0 && u32MstPathCost1 == u32MstPathCost2 && memcmp (pu8TxBridgeId1, pu8TxBridgeId2, 8) == 0 && u16TxPortId1 == u16TxPortId2 && u16RxPortId1 == u16RxPortId2) ? 0: 1;
+		
+	if (i8Cmp != -1 &&
+		memcmp (ieee8021StpBridgeId_address (pu8TxBridgeId1), ieee8021StpBridgeId_address (pu8TxBridgeId2), IeeeEui48_size_c) == 0 &&
+		ieee8021StpPortId_portNumber (u16TxPortId1) == ieee8021StpPortId_portNumber (u16TxPortId2))
+	{
+		i8Cmp = -1;
+	}
+	
+	return i8Cmp;
+}
+
 bool
 ieee8021StpPort_init (
 	ieee8021SpanningTreeEntry_t *poStp,
@@ -44,11 +84,11 @@ ieee8021StpPort_init (
 	memcpy (poEntry->au8DesignatedBridge, poStp->au8DesignatedRoot, sizeof (poEntry->au8DesignatedBridge));
 	memset (poEntry->au8DesignatedPort, 0, sizeof (poEntry->au8DesignatedPort));
 	poEntry->u64ForwardTransitions = 0;
-	//poEntry->u8RstpOperEdgePort = poEntry->u8RstpAdminEdgePort;
+	poEntry->u8RstpOperEdgePort = poEntry->u8RstpAdminEdgePort;
 	poEntry->oCist.u32Uptime = xTime_centiTime (xTime_typeMono_c);
 	memcpy (poEntry->oCist.au8DesignatedRoot, poStp->au8DesignatedRoot, sizeof (poEntry->oCist.au8DesignatedRoot));
-	//poEntry->oCist.u8OperEdgePort = poEntry->oCist.u8AdminEdgePort;
-	//poEntry->oCist.u8Disputed = ieee8021MstpCistPortDisputed_false_c;
+	poEntry->oCist.u8OperEdgePort = poEntry->oCist.u8AdminEdgePort;
+	poEntry->oCist.u8Disputed = ieee8021MstpCistPortDisputed_false_c;
 	memcpy (poEntry->oCist.au8CistRegionalRootId, poStp->au8DesignatedRoot, sizeof (poEntry->oCist.au8CistRegionalRootId));
 	poEntry->oCist.u32CistPathCost = 0;
 	
